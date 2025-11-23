2892886182b3207d80f1c252226073843fcf652a
vlagmanangelo-beep <vlagman.angelo@gmail.com>
2025-11-23 21:07:04 +0800

    ytes

--- Surveylyze_app/Static/main/Javascript/analytics_admin.js
+++ Surveylyze_app/Static/main/Javascript/analytics_admin.js
@@ -397,3 +397,24 @@ function renderMCQPieChart(q) {
     }
   });
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logoutBtn = document.querySelector(".btn.btn-outline-danger.w-100");
+  const modal = document.getElementById("logoutModal");
+  const cancel = document.getElementById("logoutCancel");
+
+  logoutBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.style.display = "flex";
+  });
+
+  cancel.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) modal.style.display = "none";
+  });
+});
+
